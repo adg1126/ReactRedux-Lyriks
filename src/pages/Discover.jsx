7e0cfe87e9b41d7571dd8f4943acf96bedf20543
Diane998 <gutierrez.almiradyan@gmail.com>
2022-10-20 13:40:31 +0800

    Wired up genre select functionality

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -18,6 +18,8 @@ const Discover = () => {
     genreListId || 'POP'
   );
 
+  const genreTitle = genres?.find(({ value }) => value === genreListId)?.title;
+
   return isFetching ? (
     <Loader title="Loading songs..." />
   ) : error ? (
@@ -25,10 +27,12 @@ const Discover = () => {
   ) : (
     <div className="flex flex-col">
       <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
-        <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+        <h2 className="font-bold text-3xl text-white text-left">
+          Discover {genreTitle || 'Pop'}
+        </h2>
         <select
           onChange={(e) => dispatch(selectGenreListId(e.target.value))}
-          value={genreListId || 'POP'}
+          value={genreListId}
           className="bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5"
         >
           {genres.map(({ title, value }) => (
